9a50f4b5b759e379df8c293e7a7b7c964b4bc233
Juan Nicolas <juanicolas@msn.com>
2018-03-28 19:50:36 -0500

    require 'will_paginate

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,5 +1,6 @@
 # Be sure to restart your server when you modify this file.
-
+require 'will_paginate/view_helpers/link_renderer'
+require 'will_paginate/view_helpers/action_view'
 # ActiveSupport::Reloader.to_prepare do
 #   ApplicationController.renderer.defaults.merge!(
 #     http_host: 'example.org',
